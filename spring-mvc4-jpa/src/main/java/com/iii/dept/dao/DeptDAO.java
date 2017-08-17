b3df779563bed1b9187e8c4dce92c7a721a56fae
dick6619 <dick6619@gmail.com>
2017-08-18 00:14:13 +0800

    OK

--- spring-mvc4-jpa/src/main/java/com/iii/dept/dao/DeptDAO.java
+++ spring-mvc4-jpa/src/main/java/com/iii/dept/dao/DeptDAO.java
@@ -2,19 +2,19 @@ package com.iii.dept.dao;
 
 import java.util.*;
 
-import com.iii.dept.model.DeptVO;
-import com.iii.emp.model.EmpVO;
+import com.iii.dept.domain.DeptVO;
+import com.iii.emp.domain.EmpVO;
 
 public interface DeptDAO {
 	DeptVO insert(DeptVO deptVO);
 
 	DeptVO getDept(Integer deptno);
 
+	List<DeptVO> getDepts();
+
 	DeptVO update(DeptVO deptVO);
 
 	void delete(Integer deptno);
-
-	List<DeptVO> getDepts();
 
 	Set<EmpVO> getEmpsByDeptno(Integer deptno);
 }
