7cfc0573772fb0a469b551b0d7d3187e32f4cab8
Lin <dick6619@gmail.com>
2017-08-23 00:38:17 +0800

    update

--- spring-mvc4-jpa/src/main/java/com/iii/emp/service/ScheduledTest.java
+++ spring-mvc4-jpa/src/main/java/com/iii/emp/service/ScheduledTest.java
@@ -1,16 +1,5 @@
 package com.iii.emp.service;
 
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ScheduledTest {
-	
-	private int i;
-	// when instance new, this method being print
-	@Scheduled(initialDelay = 10000, fixedDelay = 10000)
-	public void print() {
-		System.out.println(i++ + "******************************************************");
-	}
-
+public interface ScheduledTest {
+	void print();
 }
