e090cef308fae6ea30eeb465f093d24337ff2cff
dick6619 <dick6619@gmail.com>
2017-08-23 17:40:44 +0800

    wait

--- spring-mvc4-jpa/src/main/java/com/iii/emp/service/impl/ScheduledTestImpl.java
+++ spring-mvc4-jpa/src/main/java/com/iii/emp/service/impl/ScheduledTestImpl.java
@@ -1,5 +1,7 @@
 package com.iii.emp.service.impl;
 
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +10,16 @@ import com.iii.emp.service.ScheduledTest;
 @Service
 public class ScheduledTestImpl implements ScheduledTest {
 
+	// private static final Logger LOG =
+	// LoggerFactory.getLogger(ScheduledTestImpl.class);
 	private int i;
 
-	// when instance create, delay 1 seconds, this method execute once every 10 seconds
+	public ScheduledTestImpl() {
+		// LOG.info("ScheduledTestImpl...START");
+	}
+
+	// when instance create, delay 1 seconds, this method execute once every 10
+	// seconds
 	@Scheduled(initialDelay = 1000, fixedDelay = 10000)
 	public void print() {
 		System.out.println(++i + "******************************************************");
