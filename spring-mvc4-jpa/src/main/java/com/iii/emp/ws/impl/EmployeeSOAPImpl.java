e620bb2f64daa18732978349d14f527d8b3f4b79
Lin <dick6619@gmail.com>
2017-08-26 04:20:12 +0800

    soap

--- spring-mvc4-jpa/src/main/java/com/iii/emp/ws/impl/EmployeeSOAPImpl.java
+++ spring-mvc4-jpa/src/main/java/com/iii/emp/ws/impl/EmployeeSOAPImpl.java
@@ -8,6 +8,7 @@ import javax.jws.WebService;
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import com.iii.emp.domain.EmpVO;
 import com.iii.emp.enumeration.EmpError;
 import com.iii.emp.service.EmpService;
 import com.iii.emp.ws.EmployeeSOAP;
@@ -21,6 +22,11 @@ public class EmployeeSOAPImpl implements EmployeeSOAP {
 	@Resource(name = "empService")
 	EmpService empService;
 
+	/**
+	 * 1. 錯誤應該在empService內噴出ServiceException, 在由介接程式將ServiceException轉為錯誤代碼。傳於外機關。
+	 * 2. empService內的錯誤訊息供內部人員除錯。程式須更改。這邊程式碼會在短
+	 * 3. 應該設計為emp為一個module, ws為一個module
+	 * */
 	@Override
 	public EmpOutput getEmp(EmpInput input) {
 		//
@@ -31,7 +37,12 @@ public class EmployeeSOAPImpl implements EmployeeSOAP {
 		}
 		try {
 			//
-			BeanUtils.copyProperties(empOutput, empService.getEmp(input.getEmpno()));
+			EmpVO empVO = empService.getEmp(input.getEmpno());
+			//
+			if (empVO == null) {
+				empOutput.setStatus(EmpError.EMPTY_DATA.getCode());
+			}
+			BeanUtils.copyProperties(empOutput, empVO);
 		} catch (Exception e) {
 			empOutput.setStatus(EmpError.UNDEFINED_ERROR.getCode());
 		}
